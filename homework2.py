c07fda36f8fbad80f27a0e843734e8f682603010
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-20 10:52:23 +0300

    all file

---
+++ homework2.py
@@ -0,0 +1,7 @@
+homework_assignments = 12 # Количество выполненных ДЗ
+Time = 1.5 # Количество затраченных часов
+Course_name ='Python' # Название курса
+Time_for_one_task = 1.5/12 # Время на одно задание
+#print(Time_for_one_task) # = 0.125
+#print('python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.') =str
+print(Course_name +', всего задач:',homework_assignments,', затрачено часов:',Time,', среднее время выполнения',Time_for_one_task,'часа')
