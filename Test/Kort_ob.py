0448ec523dc7157ab1017b1edeed930cf422ac4d
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-20 11:18:20 +0300

    Create Kort_ob.py

---
+++ Test/Kort_ob.py
@@ -0,0 +1,12 @@
+#tuple_ = 1, 2, 3, 4
+#print(type (tuple_))
+#tuple_2 = (1, 2, 3, 4)
+#print(tuple_2)
+#tuple_3 = tuple([1, 2, 3, 4])
+#print(tuple_3)
+
+
+tuple_ = ([1, 2], 0, 4, 5)
+print(tuple_)
+tuple_[0][0] =2
+print(tuple_)
