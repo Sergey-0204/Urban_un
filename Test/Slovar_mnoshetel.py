c93d7013e4d47335682bb06f9ebfc692cd676f9e
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-20 11:19:22 +0300

    Create Slovar_mnoshetel.py

---
+++ Test/Slovar_mnoshetel.py
@@ -0,0 +1,19 @@
+# "Словарь"
+#phone_book ={"Sergey": 89032889550, 'Denis':80055544433, 'max':89035553335}
+#print(phone_book)
+#phone_book ['anton'] = 89036668541
+#print(phone_book)
+#phone_book.update({'Sasha': 1535556985,
+             #      'Alex': 1234567854})
+#print(phone_book)
+#a = phone_book.pop ('Sergey')
+#print(a)
+#print(phone_book.keys())
+#print(phone_book.items())
+
+
+# "Множества"
+set_ = {1, 2, 3, 4, 5, 1, 2, 3, 4, 'string' , True, (1,2)}
+print(set_)
+List_ =[1, 1, 1, 2, 2, 3, 3]
+print(set(List_))
