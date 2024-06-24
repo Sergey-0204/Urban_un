e951ecfd663d6079b3bc9577e4a38e5f4dcdb030
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-24 12:23:42 +0300

    all file

--- Test/datas.py
+++ Test/datas.py
@@ -1,5 +1,5 @@
-print(type('hello,word!')) #=str
-print(type(100)) #=int
-print(type(500<100)) #=bool (true, false)
+print(type('hello,word!'))  # = str
+print(type(100))  # = int
+print(type(500 < 100))  # = bool (true11, false)
 primer = not True
 print(primer)
--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -5,19 +5,19 @@ print(first)
 print(second)
 print(third)
 if first == second == third:
-  print(3)
+    print(3)
 else:
-  print(0)
-  first = 34
-  second = 50
-  third = 34
+    print(0)
+first = 34
+second = 50
+third = 34
 print(first)
 print(second)
 print(third)
 if first == second or second == third or first == third:
-  print(2)
+    print(2)
 else:
-  print(0)
+    print(0)
 first = 100
 second = 100
 third = 100
@@ -25,6 +25,6 @@ print(first)
 print(second)
 print(third)
 if first == second == third:
-  print(3)
+    print(3)
 else:
-  print(0)
+    print(0)
