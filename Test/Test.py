3435ff2c9446788f9c71297429be44437f8772f6
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-24 09:35:55 +0300

    all file

--- Test/Test.py
+++ Test/Test.py
@@ -1,18 +1,4 @@
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_list = list(students)
-students_list.sort()
-students_list = ['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
-Aaron = sum(grades[0])/len(grades[0])
-Bilbo = sum(grades[1])/len(grades[1])
-print(Bilbo)
-Johnny = sum(grades[2])/len(grades[2])
-print(Johnny)
-Khendrik = sum(grades[3])/len(grades[3])
-print(Khendrik)
-Steve = sum(grades[4])/len(grades[4])
-print(Steve)
-print(Aaron, Bilbo, Johnny, Khendrik,Steve)
+
 
 
 
