88bc9b1491115be72a784f797caadce9513400de
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-24 17:51:23 +0300

    all file

--- Test/module_2_3.py
+++ Test/module_2_3.py
@@ -1,5 +1,7 @@
 my_list = [42, 69, 322, 13, 99, -5, 9, 8, 7, -6, 5]
 zero = [0]
-len(my_list[0:4])
-print(my_list)
-#while 1 > 0:
+print (len(my_list[0:4]))
+while 1 > 0:
+    if my_list:
+        print(my_list)
+
