ee6740e0a441f0a76ddcf47ea5809682cce85b47
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-24 09:38:51 +0300

    all file

---
+++ module_2/module_2_2.py
@@ -0,0 +1,30 @@
+first = 127
+second = 364
+third = 534
+print(first)
+print(second)
+print(third)
+if first == second == third:
+  print(3)
+else:
+  print(0)
+  first = 34
+  second = 50
+  third = 34
+print(first)
+print(second)
+print(third)
+if first == second or second == third or first == third:
+  print(2)
+else:
+  print(0)
+first = 100
+second = 100
+third = 100
+print(first)
+print(second)
+print(third)
+if first == second == third:
+  print(3)
+else:
+  print(0)
