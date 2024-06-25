5df75e8369dff020ac467584de5e4aef60b3fb73
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-25 10:47:15 +0300

    all file

---
+++ module_2/module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 0, 13, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+
+while index < len(my_list):
+
+    if my_list[index] > 0:
+        print(my_list[index])
+    elif my_list[index] == 0:
+        var = - index
+    else:
+        break
+    index += 1
