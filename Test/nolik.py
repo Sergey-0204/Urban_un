3ec99a17585185be882616b100255a2893c48c1a
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-27 12:57:31 +0300

    all file

--- Test/nolik.py
+++ Test/nolik.py
@@ -24,8 +24,42 @@ for turn in range(1, 10):
         print('Ячейка уже занята, вы пропускаете ход')
         draw_area()
         continue
-draw_area()
-area[0][0] =='X' and area[0][1] =='X' and area[0][2] # проверка
-
+def draw_area():
+    if area[0][0] =='X' and area[0][1] =='X' and area[0][2] == "X":
+        return "X"
+    if area[1][0] =='X' and area[1][1] =='X' and area[1][2] == "X":
+        return "X"
+    if area[1][0] =='X' and area[1][1] =='X' and area[1][2] == "X":
+        return "X"
+    if area[2][0] =='X' and area[2][1] =='X' and area[2][2] == "X":
+        return "X"
+    if area[0][0] =='X' and area[1][0] =='X' and area[2][0] == "X":
+        return "X"
+    if area[0][1] =='X' and area[1][1] =='X' and area[2][1] == "X":
+        return "X"
+    if area[0][2] =='X' and area[1][2] =='X' and area[2][2] == "X":
+        return "X"
+    if area[0][0] =='X' and area[1][1] =='X' and area[2][2] == "X":
+        return "X"
+    if area[0][2] =='X' and area[1][1] =='X' and area[2][0] == "X":
+        return "X"
+    if area[0][0] =='0' and area[0][1] =='0' and area[0][2] == "0":
+        return "0"
+    if area[1][0] =='0' and area[1][1] =='0' and area[1][2] == "0":
+        return "0"
+    if area[1][0] =='0' and area[1][1] =='0' and area[1][2] == "0":
+        return "0"
+    if area[2][0] =='0' and area[2][1] =='0' and area[2][2] == "0":
+        return "0"
+    if area[0][0] =='0' and area[1][0] =='0' and area[2][0] == "0":
+        return "0"
+    if area[0][1] =='0' and area[1][1] =='0' and area[2][1] == "0":
+        return "0"
+    if area[0][2] =='0' and area[1][2] =='0' and area[2][2] == "0":
+        return "0"
+    if area[0][0] =='0' and area[1][1] =='0' and area[2][2] == "0":
+        return "0"
+    if area[0][2] =='0' and area[1][1] =='0' and area[2][0] == "0":
+        return "0"
 
 draw_area()
