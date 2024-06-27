1be62113b9fd73c26e29e0cfaccc55a80a0fc525
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-06-27 13:39:16 +0300

    all file

--- Test/nolik.py
+++ Test/nolik.py
@@ -1,10 +1,13 @@
 import area
+
+
 def draw_area():
     for i in area:
         print(*i)
     print()
 
-area = [["*", "*", "*",], ["*", "*", "*",], ["*", "*", "*"]]
+
+area = [["*", "*", "*", ], ["*", "*", "*", ], ["*", "*", "*"]]
 print('Крестики-нолики')
 print('______________________')
 draw_area()
@@ -14,52 +17,55 @@ for turn in range(1, 10):
         turn_char = '0'
         print('Ходят нолики')
     else:
-        turn_char ='X'
+        turn_char = 'X'
         print('Ходят крестики')
-    row = int(input("Введите номер строки 1,2,3 ")) -1
-    column = int(input("Введите номер столбца 1,2,3 ")) -1
-    if area[row][column] =="*":
+    row = int(input("Введите номер строки 1,2,3 ")) - 1
+    column = int(input("Введите номер столбца 1,2,3 ")) - 1
+    if area[row][column] == "*":
         area[row][column] = turn_char
     else:
         print('Ячейка уже занята, вы пропускаете ход')
         draw_area()
         continue
+
+
 def draw_area():
-    if area[0][0] =='X' and area[0][1] =='X' and area[0][2] == "X":
+    if area[0][0] == 'X' and area[0][1] == 'X' and area[0][2] == "X":
         return "X"
-    if area[1][0] =='X' and area[1][1] =='X' and area[1][2] == "X":
+    if area[1][0] == 'X' and area[1][1] == 'X' and area[1][2] == "X":
         return "X"
-    if area[1][0] =='X' and area[1][1] =='X' and area[1][2] == "X":
+    if area[1][0] == 'X' and area[1][1] == 'X' and area[1][2] == "X":
         return "X"
-    if area[2][0] =='X' and area[2][1] =='X' and area[2][2] == "X":
+    if area[2][0] == 'X' and area[2][1] == 'X' and area[2][2] == "X":
         return "X"
-    if area[0][0] =='X' and area[1][0] =='X' and area[2][0] == "X":
+    if area[0][0] == 'X' and area[1][0] == 'X' and area[2][0] == "X":
         return "X"
-    if area[0][1] =='X' and area[1][1] =='X' and area[2][1] == "X":
+    if area[0][1] == 'X' and area[1][1] == 'X' and area[2][1] == "X":
         return "X"
-    if area[0][2] =='X' and area[1][2] =='X' and area[2][2] == "X":
+    if area[0][2] == 'X' and area[1][2] == 'X' and area[2][2] == "X":
         return "X"
-    if area[0][0] =='X' and area[1][1] =='X' and area[2][2] == "X":
+    if area[0][0] == 'X' and area[1][1] == 'X' and area[2][2] == "X":
         return "X"
-    if area[0][2] =='X' and area[1][1] =='X' and area[2][0] == "X":
+    if area[0][2] == 'X' and area[1][1] == 'X' and area[2][0] == "X":
         return "X"
-    if area[0][0] =='0' and area[0][1] =='0' and area[0][2] == "0":
+    if area[0][0] == '0' and area[0][1] == '0' and area[0][2] == "0":
         return "0"
-    if area[1][0] =='0' and area[1][1] =='0' and area[1][2] == "0":
+    if area[1][0] == '0' and area[1][1] == '0' and area[1][2] == "0":
         return "0"
-    if area[1][0] =='0' and area[1][1] =='0' and area[1][2] == "0":
+    if area[1][0] == '0' and area[1][1] == '0' and area[1][2] == "0":
         return "0"
-    if area[2][0] =='0' and area[2][1] =='0' and area[2][2] == "0":
+    if area[2][0] == '0' and area[2][1] == '0' and area[2][2] == "0":
         return "0"
-    if area[0][0] =='0' and area[1][0] =='0' and area[2][0] == "0":
+    if area[0][0] == '0' and area[1][0] == '0' and area[2][0] == "0":
         return "0"
-    if area[0][1] =='0' and area[1][1] =='0' and area[2][1] == "0":
+    if area[0][1] == '0' and area[1][1] == '0' and area[2][1] == "0":
         return "0"
-    if area[0][2] =='0' and area[1][2] =='0' and area[2][2] == "0":
+    if area[0][2] == '0' and area[1][2] == '0' and area[2][2] == "0":
         return "0"
-    if area[0][0] =='0' and area[1][1] =='0' and area[2][2] == "0":
+    if area[0][0] == '0' and area[1][1] == '0' and area[2][2] == "0":
         return "0"
-    if area[0][2] =='0' and area[1][1] =='0' and area[2][0] == "0":
+    if area[0][2] == '0' and area[1][1] == '0' and area[2][0] == "0":
         return "0"
+
 
 draw_area()
