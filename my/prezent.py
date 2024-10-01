b735093c916418f438002723b23f7ac66aa30a68
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-10-01 12:48:56 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ my/prezent.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Данные
+municipal_districts = ['Балашиха', 'Богородский', 'Восход', 'Жуковский', 'Реутов', 'Абдуллино']
+connected = [0, 33, 1, 36, 0, 33]
+not_connected = [36, 3, 35, 0, 36, 3]
+
+x = np.arange(len(municipal_districts))
+width = 0.6
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Создание столбцов
+ax.bar(x, connected, width, label='Подключено', color='green')
+ax.bar(x, not_connected, width, bottom=connected, label='Не подключено', color='red')
+
+# Настройка осей и заголовка
+ax.set_ylabel('Всего количество ноутбуков')
+ax.set_title('Подключение образовательных организаций по городским округам')
+ax.set_xticks(x)
+ax.set_xticklabels(municipal_districts, rotation=45)
+ax.legend()
+
+# Добавление подписей данных
+for i in range(len(municipal_districts)):
+    ax.text(x[i], connected[i]/2, connected[i], ha='center', va='center', color='white', fontsize=10)
+    ax.text(x[i], connected[i] + not_connected[i]/2, not_connected[i], ha='center', va='center', color='white', fontsize=10)
+
+plt.tight_layout()
+plt.show()
