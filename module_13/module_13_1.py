842fbd5a63b193a22a053ebe93e0a909a95108af
Sergey-0204 <91122600+Sergey-0204@users.noreply.github.com>
2024-10-24 13:49:30 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ module_13/module_13_1.py
@@ -0,0 +1,28 @@
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+
+    for i in range(1, 6):  # 5 шаров
+        await asyncio.sleep(1 / power)  # Задержка обратно пропорциональна силе
+        print(f'Силач {name} поднял {i} шар')
+
+    print(f'Силач {name} закончил соревнования.')
+
+
+async def start_tournament():
+    # Создаем задачи для трех силачей
+    tasks = [
+        start_strongman('Pasha', 3),
+        start_strongman('Denis', 4),
+        start_strongman('Apollon', 5)
+    ]
+
+    # Ждем завершения всех задач
+    await asyncio.gather(*tasks)
+
+
+# Запуск асинхронной функции
+if __name__ == "__main__":
+    asyncio.run(start_tournament())
